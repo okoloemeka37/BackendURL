97a56e0792935ca0c169e789a9b8ae456d30c853
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-06-26 14:09:44 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2'
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "url_shortener",
+  host: "bqrljmw02erm9bzpdevn-mysql.services.clever-cloud.com",
+  user: "u0cpcsrsq3pvfgdj",
+  password: "Fow8u3OMWzXLc0L8X1ba",
+  database: "bqrljmw02erm9bzpdevn",
 });
 
 db.connect((err) => {
