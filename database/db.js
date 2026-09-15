8535f3bf49eb36f42a438d30c458ae6bd00f7e44
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-09-15 03:18:28 +0100

    up

--- database/db.js
+++ database/db.js
@@ -1,15 +1,22 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "url_shortener",
+  host: process.env.DB_HOST || "gateway01.eu-central-1.prod.aws.tidbcloud.com",
+  user: process.env.DB_USER || "2ZctqwexiZdJy6h.root",
+  port: Number(process.env.DB_PORT) || 4000,
+  password: process.env.DB_PASSWORD || "A81axlancHvD0IIL",
+  database: process.env.DB_NAME || "urlShort",
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+  // Pool Settings for Serverless & TiDB
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-});
-
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+}); 
 // Optional: Test the pool connection on startup
 try {
   const connection = await db.getConnection();
