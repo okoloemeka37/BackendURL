94e7175ccf884df5308877b0ce42b2a0a48ef045
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2026-09-16 03:50:13 +0100

    Update UrlRouter.js

--- routes/UrlRouter.js
+++ routes/UrlRouter.js
@@ -32,7 +32,8 @@ const clip =data.clip;
         const original=url.href
         const hostName=url.hostname;
        // const shortURL=`https://zyler.com.ng/${slug}`;
-        const shortURL=`http://localhost:3000/${slug}`
+        //const shortURL=`http://localhost:3000/${slug}`
+        const shortURL=`https://frontend-url-blush.vercel.app/${slug}`
       const result=await db.query(`INSERT INTO links(userId,short,original,clicks) VALUES(?,?,?,?)`,[userId,slug,original,0])
         console.log(result)
          return res.status(200).json({shortURL})
